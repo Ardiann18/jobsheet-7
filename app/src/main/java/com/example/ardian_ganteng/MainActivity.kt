5fe883befd819ffddc1f21d1c723403fe639b2b7
chelsea <chelsea@gmail.com>
2022-08-08 13:14:28 +0700

    JOBSHEET 7

---
+++ app/src/main/java/com/example/ardian_ganteng/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ardian_ganteng
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
